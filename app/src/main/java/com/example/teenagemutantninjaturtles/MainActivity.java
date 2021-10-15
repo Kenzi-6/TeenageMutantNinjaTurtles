6bfe596f9055e7eb19e8307c6ad3d0a5f25c0960
Kenzi-6 <sanek.samoldin@mail.ru>
2021-10-15 19:19:36 +0300

    firstUpdate

--- app/src/main/java/com/example/teenagemutantninjaturtles/MainActivity.java
+++ app/src/main/java/com/example/teenagemutantninjaturtles/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.teenagemutantninjaturtles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView heroesList = (ListView) findViewById(R.id.heroesList);
+
+        // получаем ресурс
+        String[] heroes = getResources().getStringArray(R.array.heroes);
+
+        // создаем адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, heroes);
+
+        // устанавливаем для списка адаптер
+        heroesList.setAdapter(adapter);
     }
 }
