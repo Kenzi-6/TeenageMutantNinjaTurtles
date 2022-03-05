7ae0e83baa4d8f7eb1998251dbb58a2469d2494f
Kenzi-6 <sanek.samoldin@mail.ru>
2022-03-05 12:05:37 +0300

    good work well done

--- app/src/main/java/com/example/teenagemutantninjaturtles/MainActivity2.java
+++ app/src/main/java/com/example/teenagemutantninjaturtles/MainActivity2.java
@@ -16,47 +16,38 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        TextView textView = findViewById(R.id.heroesList2);
         WebView browser = findViewById(R.id.webBrowser);
         String heroVar = getIntent().getStringExtra("hero");
         switch (heroVar) {
             case ("Леонардо"):
-                textView.setText(heroVar);
                 browser.setWebViewClient(new WebViewClient());
                 browser.loadUrl("https://turtlepedia.fandom.com/ru/wiki/Леонардо_(мультсериал_2003)");
                 break;
             case ("Рафаэль"):
-                textView.setText(heroVar);
                 browser.setWebViewClient(new WebViewClient());
                 browser.loadUrl("https://turtlepedia.fandom.com/ru/wiki/Рафаэль_(мультсериал_2003)");
                 break;
             case ("Донателло"):
-                textView.setText(heroVar);
                 browser.setWebViewClient(new WebViewClient());
                 browser.loadUrl("https://turtlepedia.fandom.com/ru/wiki/Донателло_(мультсериал_2003)");
                 break;
             case ("Микеланджело"):
-                textView.setText(heroVar);
                 browser.setWebViewClient(new WebViewClient());
                 browser.loadUrl("https://turtlepedia.fandom.com/ru/wiki/Микеланджело_(мультсериал_2003)");
                 break;
             case ("Сплинтер"):
-                textView.setText(heroVar);
                 browser.setWebViewClient(new WebViewClient());
                 browser.loadUrl("https://turtlepedia.fandom.com/ru/wiki/Сплинтер_(мультсериал_2003)");
                 break;
             case ("Эйприл О'Нил"):
-                textView.setText(heroVar);
                 browser.setWebViewClient(new WebViewClient());
                 browser.loadUrl("https://turtlepedia.fandom.com/ru/wiki/Эйприл_О%27Нил_(мультсериал_2003)");
                 break;
             case ("Кейси Джонс"):
-                textView.setText(heroVar);
                 browser.setWebViewClient(new WebViewClient());
                 browser.loadUrl("https://turtlepedia.fandom.com/ru/wiki/Арнольд_Кейси_Джонс_мл._(мультсериал_2003)");
                 break;
             case ("Шреддер"):
-                textView.setText(heroVar);
                 browser.setWebViewClient(new WebViewClient());
                 browser.loadUrl("https://turtlepedia.fandom.com/ru/wiki/Ч%27релл_(мультсериал_2003)");
                 break;
